652bba94b409ec9870c679d7a6b8a830effdba39
Christopher Eeles <christopher.eeles@outlook.com>
2022-06-08 17:28:56 -0400

    Overhauled prediction function to consider both the correlation and whether a gene is up/down relative tot he population

--- inst/shiny-scripts/univariateTab.R
+++ inst/shiny-scripts/univariateTab.R
@@ -15,19 +15,19 @@ univariateTabInitialize <- function(input, output, globalRV) {
 univariateTabUI <- div(
   br(),
   div(class='flex', style='align-items: stretch',
-      div(style='width: 33%', 
+      div(style='width: 33%',
           # p("Please upload your reference population file"),
           fileInput("referencePopulationDf",
                     "",
                     accept=c("text/csv", ".csv"), buttonLabel="Browse files"),
           createTip("Tip", "File format must be .csv"),
       ),
-      div(style='width: 67%; background-color: #ecf0f1; border-radius: 8px; padding: 8px;', 
+      div(style='width: 67%; background-color: #ecf0f1; border-radius: 8px; padding: 8px;',
           uiOutput('selectedRow'),
       )
   ),
   br(),
-  
+
   dataTableOutput("pdbBiomarkers"),
 )
 
@@ -42,7 +42,7 @@ univariateTabCreateRV <- function() {
         system.file("extdata", package="PGxVision"),
         "sample_data",
         "pharmacodb_biomarker_df.csv"
-      ))), 
+      ))),
     univarResultsDf=NULL,
   ))
 }
@@ -66,8 +66,8 @@ univariateTabOutputUI <- function (input, rv, output, globalRV) {
     else {
       index <- input$pdbBiomarkers_rows_selected
       if (identical(index, NULL)) { return(p("Select a row to learn more."))}
-      
-      # Find reevant pathways 
+
+      # Find reevant pathways
       gene <- rv$univarResultsDf[index,]$gene_symbol
       relevantPathways <- globalRV$ssGseaResults[grepl(pattern=gene, globalRV$ssGseaResults$genes), ]
 
@@ -78,7 +78,7 @@ univariateTabOutputUI <- function (input, rv, output, globalRV) {
               div(span(style='font-weight: bold', "Gene Set Category"), span(globalRV$geneSetCategory)),
               div(span(style='font-weight: bold', 'Enriched Pathways')),
               div(style='display: flex; flex-wrap: wrap;',
-                  lapply(rownames(relevantPathways), \(x) { 
+                  lapply(rownames(relevantPathways), \(x) {
                     return (
                       div(style='background-color: white; padding: 1px 8px; word-wrap: break-word;
                       border-radius: 15px; margin: 2px 4px 2px 0px; display: inline-block',
@@ -95,39 +95,50 @@ univariateTabOutputUI <- function (input, rv, output, globalRV) {
             )
         ))
     }})
-  
+
   output$pdbBiomarkers <- DT::renderDataTable({
     pdf <- rv$patientDf
     rdf <- rv$referenceDf
     biodf <- data.frame(rv$pdbBiomarkersDf)
-    
-    # Return empty table if files not uploaded 
+
+    # Return empty table if files not uploaded
     if (identical(pdf, NULL) | identical(rdf, NULL)) {
       return(data.frame(Biomarkers=character()))
     }
-    
+
     df_ <- PGxVision::retrieveDiffExpressedGenes(pdf, rdf, biodf)
-    
+
     # Add emojis to show direction of correlation
     appendSmileys <- function(x) {
-      if (x$estimate > 0) { return ("✅ Sensitive")} 
-      else if (x$estimate < 0) {return ("❌ Resistant")}
-      else {return(" ")}}
+      positive_corr <- x$estimate > 0
+      percentile_high <- x$percentile > 0.9
+      percentile_low <- x$percentile < 0.1
+      if ((positive_corr && percentile_high) || (!positive_corr && percentile_low)) {
+        return("✅ Sensitive")
+      } else if ((!positive_corr && percentile_high) || (positive_corr && percentile_low)) {
+        return("❌ Resistant")
+      } else {
+        return("❔Neither")
+      }
+    }
     correlation_direction <- apply(df_, MARGIN = 1, appendSmileys)
     df_ <- cbind(df_, correlation_direction)
-    
-    # Make the numbers round to 4 decimals
-    df_$estimate <- round(as.numeric(df_$estimate), 4)
-    df_$pvalue <- sprintf("%.4e", as.numeric(df_$pvalue)) 
-    df_$percentile <- round(as.numeric(df_$percentile), 4)
-    
+
+    # Make the numbers round to 3 decimals
+    df_$estimate <- round(as.numeric(df_$estimate), 3)
+    df_$pvalue <- sprintf("%.2e", as.numeric(df_$pvalue))
+    df_$percentile <- round(as.numeric(df_$percentile), 3)
+    df_$score <- round(df_$score, 3)
+
     # Order the dataframe and add more human column names
-    df_ <- df_[order(df_$pvalue, -abs(df_$estimate)), c('compound_name', 'gene_symbol', 'estimate', 'correlation_direction', 
-                                                        'pvalue', 'percentile', 'pubchem', 'tissue')]
+    df_ <- df_[order(abs(df_$score), decreasing=TRUE),
+      c('compound_name', 'gene_symbol', 'correlation_direction', 'score',
+        'estimate', 'pvalue', 'percentile', 'pubchem', 'tissue')]
     rv$univarResultsDf <- df_
-    colnames(df_) <- c('Compound Name', 'Gene', 'Correlation', 'Correlation Direction', 'P-value', 
-                       'Sample Expression Percentile', 'PubChem ID', 'Tissue Type')
+    colnames(df_) <- c(
+      'Compound Name', 'Gene', 'Prediction',  'Prediction Score', 'Correlation',
+      'Corr. P-value', 'Sample Expression Percentile', 'PubChem ID', 'Tissue Type')
     return (df_)
-    
+
   }, selection='single')
 }
